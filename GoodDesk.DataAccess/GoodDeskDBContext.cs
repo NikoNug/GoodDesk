0a58da740a39ef8a5b5c32d420ef1ffe77e7b23a
NikoNug <nikonugraha03@gmail.com>
2026-09-07 23:31:12 +0700

    GD-008 GD-009

--- GoodDesk.DataAccess/GoodDeskDBContext.cs
+++ GoodDesk.DataAccess/GoodDeskDBContext.cs
@@ -1,5 +1,6 @@
 ﻿using GoodDesk.DataModel;
 using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices.Marshalling;
 
 namespace GoodDesk.DataAccess
 {
@@ -11,6 +12,46 @@ namespace GoodDesk.DataAccess
         { 
         }
 
-        public virtual DbSet<TblMCategory> TblMCategories { get; set; } = null!;
+        public virtual DbSet<TBLMCategory> TBLMCategories { get; set; } = null!;
+        public virtual DbSet<TBLMRole> TBLMRoles { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TBLMUser>()
+                .HasOne(u => u.Role)
+                .WithMany()
+                .HasForeignKey(u => u.RoleID)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<TBLMRole>().HasData(
+                new TBLMRole
+                {
+                    ID = 1,
+                    RoleName = "Admin",
+                    Description = "System Administrator",
+                    IsActive = true,
+                    CreatedBy = 1,
+                    CreatedDate = new DateTime(2026, 1, 1)
+                },
+                new TBLMRole
+                {
+                    ID = 2,
+                    RoleName = "IT Support",
+                    Description = "IT support staff",
+                    IsActive = true,
+                    CreatedBy = 1,
+                    CreatedDate = new DateTime(2026, 1, 1)
+                },
+                new TBLMRole
+                {
+                    ID = 3,
+                    RoleName = "Requester",
+                    Description = "User who creates support tickets",
+                    IsActive = true,
+                    CreatedBy = 1,
+                    CreatedDate = new DateTime(2026, 1, 1)
+                }
+            );
+        }
     }
 }
--- GoodDesk.DataModel/TBLMRole.cs
+++ GoodDesk.DataModel/TBLMRole.cs
@@ -1,21 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace GoodDesk.DataModel
 {
-    [Table("TBL_M_Categories")]
-    public class TBLMCategory
+    [Table("TBL_M_Roles")]
+    public class TBLMRole
     {
+        [Key]
         public int ID { get; set; }
-        public string CategoryName { get; set; } = null!;
+
+        [Required, MaxLength(50)]
+        public string RoleName { get; set; } = null!;
+
+        [MaxLength(255)]
         public string? Description { get; set; }
-        public bool IsDeleted {  get; set; }
+
+        public bool IsActive { get; set; }
+
         public int CreatedBy { get; set; }
+
         public DateTime CreatedDate { get; set; }
+
         public int? UpdatedBy { get; set; }
+
         public DateTime? UpdatedDate { get; set; }
 
+        public ICollection<TBLMUser> Users { get; set; } = new List<TBLMUser>();
     }
 }
